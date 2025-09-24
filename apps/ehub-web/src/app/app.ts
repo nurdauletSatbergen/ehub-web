85e3772f5f51056164e2cfa9f19ce2420c81eb8e
Nurdaulet <nurik93sat@gmail.com>
2025-09-25 02:26:10 +0500

    Added material ui

--- apps/ehub-web/src/app/app.ts
+++ apps/ehub-web/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { MatButton } from '@angular/material/button';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, MatButton],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
